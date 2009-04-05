1b9b917f58de6bc76db298465fb86ab99192cf10
delano <delano@solutious.com>
2009-04-04 22:41:19 -0400

    Updated command whitelist

--- lib/rye/cmd.rb
+++ lib/rye/cmd.rb
@@ -22,25 +22,37 @@ module Rye;
     def cp(*args); cmd("cp", args); end
     def mv(*args); cmd("mv", args); end
     def ls(*args); cmd('ls', args); end
-    def rm(*args); cmd('rm', args); end
+    #def rm(*args); cmd('rm', args); end
     def ps(*args); cmd('ps', args); end
     def sh(*args); cmd('sh', args); end
+    
     def env; cmd "env"; end
     def pwd; cmd "pwd"; end
+    def svn(*args); cmd('svn', args); end
+    def cvs(*args); cmd('cvs', args); end
+    def git(*args); cmd('git', args); end
+    def sed(*args); cmd('sed', args); end
+    def awk(*args); cmd('awk', args); end
     def cat(*args); cmd('cat', args); end
+    
+    #def kill(*args); cmd('kill', args); end
+    def sudo(*args); cmd('sudo', args); end
     def grep(*args); cmd('grep', args); end
     def date(*args); cmd('date', args); end
     def ruby(*args); cmd('ruby', args); end
     def perl(*args); cmd('perl', args); end
     def bash(*args); cmd('bash', args); end
     def echo(*args); cmd('echo', args); end
+    
+    def mount; cmd("mount"); end
     def sleep(seconds=1); cmd("sleep", seconds); end
     def touch(*args); cmd('touch', args); end
     def uname(*args); cmd('uname', args); end
-    def mount; cmd("mount"); end
-    def python(*args); cmd('python', args); end
+    
     def uptime; cmd("uptime"); end
+    def python(*args); cmd('python', args); end
     def printenv(*args); cmd('printenv', args); end
+    
     # Consider Rye.sysinfo.os == :unix
   end
 
