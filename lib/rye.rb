0e18131adfeca1158a473ebe9971390b4745b149
delano <delano@solutious.com>
2009-04-04 22:49:55 -0400

    Updated slogan

--- lib/rye.rb
+++ lib/rye.rb
@@ -9,12 +9,12 @@ require 'sys'
 
 # = Rye
 #
-# Run system commands via SSH locally and remotely in a Ruby way.
+# Safely run remote commands via SSH in Ruby.
 #
 # Rye is similar to Rush[http://rush.heroku.com] but everything 
 # happens over SSH (no HTTP daemon) and the default settings are
-# less powerful (for safety). For example, file globs are disabled 
-# so unless otherwise specified, you can't do this: 
+# less dangerous (for safety). For example, file globs are  
+# disabled so unless otherwise specified, you can't do this: 
 # <tt>rbox.rm('/etc/**/*')</tt>. 
 #
 # * See +bin/try+ for a bunch of working examples. 
