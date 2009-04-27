695939dd00cf433d482165ff355948c8641ade20
delano <delano@solutious.com>
2009-04-27 09:02:36 -0400

    Added file transfer example to docs

--- tst/60_rbox_transfer_test.rb
+++ tst/60_rbox_transfer_test.rb
@@ -4,47 +4,48 @@
 # Usage: test/60_rbox_upload_test.rb
 #
 
-$:.unshift File.join(File.dirname(__FILE__), '..', 'lib')
+$:.unshift File.join(File.dirname(__FILE__), "..", "lib")
 
-require 'rubygems'
-require 'stringio'
-require 'yaml'
-require 'rye'
+require "rubygems"
+require "stringio"
+require "yaml"
+require "rye"
 
+tmpdir = Rye.sysinfo.tmpdir
 
-rbox = Rye::Box.new('localhost', :info => true)
-rbox.rm(:r, :f, '/tmp/rye-upload') # Silently delete test dir
+rbox = Rye::Box.new("localhost", :info => true)
+rbox.rm(:r, :f, "#{tmpdir}/rye-upload") # Silently delete test dir
 
 # /tmp/rye-upload will be created if it doesn't exist
-rbox.upload('README.rdoc', 'LICENSE.txt', '/tmp/rye-upload')
+rbox.upload("README.rdoc", "LICENSE.txt", "#{tmpdir}/rye-upload")
 
 # A single file can be renamed
-rbox.upload('README.rdoc', '/tmp/rye-upload/README-renamed')
+rbox.upload("README.rdoc", "#{tmpdir}/rye-upload/README-renamed")
 
 # StringIO objects can be sent as files
 applejack = StringIO.new
-applejack.puts "1What's happening Applejack?"
-applejack.puts "2What's happening Applejack?"
-rbox.upload(applejack, '/tmp/rye-upload/applejack')
+applejack.puts "Delano: What's happening Applejack?"
+applejack.puts "Applejack: Just trying to get by."
+rbox.upload(applejack, "#{tmpdir}/rye-upload/applejack")
 
-#rbox.upload('tst/60-file.mp3', '/tmp/rye-upload')  # demonstrates
-#rbox.upload('tst/60-file.mp3', '/tmp/rye-upload')  # progress
-#rbox.upload('tst/60-file.mp3', '/tmp/rye-upload')  # bar
+rbox.upload("tst/60-file.mp3", "#{tmpdir}/rye-upload")  # demonstrates
+rbox.upload("tst/60-file.mp3", "#{tmpdir}/rye-upload")  # progress
+rbox.upload("tst/60-file.mp3", "#{tmpdir}/rye-upload")  # bar
 
 puts "Content of /tmp/rye-upload"
-puts rbox.ls(:l, '/tmp/rye-upload')
+puts rbox.ls(:l, "#{tmpdir}/rye-upload")
 
-rbox.download('/tmp/rye-upload/README.rdoc', 
-              '/tmp/rye-upload/README-renamed', '/tmp/rye-download')
+rbox.download("#{tmpdir}/rye-upload/README.rdoc", 
+              "#{tmpdir}/rye-upload/README-renamed", "#{tmpdir}/rye-download")
 
 # You can't download a StringIO object. This raises an exception:
-#rbox.download(applejack, '/tmp/rye-download/applejack') 
+#rbox.download(applejack, "#{tmpdir}/rye-download/applejack") 
 # But you can download _to_ a StringIO object
 applejack = StringIO.new
-rbox.download('/tmp/rye-upload/applejack', applejack)
+rbox.download("#{tmpdir}/rye-upload/applejack", applejack)
 
-puts "Content of /tmp/rye-download"
-puts rbox.ls(:l, '/tmp/rye-download')
-puts "Content of applejack StringIO object"
-applejack.rewind
+puts $/, "Content of /tmp/rye-download"
+puts rbox.ls(:l, "#{tmpdir}/rye-download")
+
+puts $/, "Content of applejack StringIO object"
 puts applejack.read
