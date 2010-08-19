ba9eb3ef37a4ddf2666ca7d03696b64881fa2632
git <delano@solutious.com>
2010-08-19 11:54:53 -0400

    File tryout

---
+++ try/15_file_tryouts.rb
@@ -0,0 +1,12 @@
+require 'rye'
+
+
+## knows dirs exist
+lbox = Rye::Box.new
+lbox.file_exists? '/etc'
+#=> true
+
+## knows files exist
+lbox = Rye::Box.new 'localhost'
+lbox.file_exists? '/etc/host'
+#=> true
