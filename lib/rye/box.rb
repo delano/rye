c1c049f4a0da03df1f033c38e146061b6b09c634
delano <delano@solutious.com>
2010-10-25 17:25:40 -0400

    ADDED: interactive_ssh adds private paths to ssh command

--- lib/rye/box.rb
+++ lib/rye/box.rb
@@ -260,9 +260,12 @@ module Rye
     #
     # TODO: refactor to use net_ssh_exec! in 0.9
     def interactive_ssh(run=true)
-      debug "interactive_ssh with keys: #{Rye.keys.inspect}"
-      run = false unless STDIN.tty?      
-      cmd = Rye.prepare_command("ssh", "#{@rye_user}@#{@rye_host}")
+      debug "interactive_ssh with keys: #{@rye_opts[:keys].inspect}"
+      run = false unless STDIN.tty?
+      args = []
+      @rye_opts[:keys].each { |key| args.push *[:i, key] }
+      args << "#{@rye_user}@#{@rye_host}"
+      cmd = Rye.prepare_command("ssh", args)
       return cmd unless run
       system(cmd)
     end
