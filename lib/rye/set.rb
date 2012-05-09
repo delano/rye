cb1e913943badc4b5cb7807e59aca8eef042ab5a
Dan Richert <dan.richert@gmail.com>
2012-05-09 11:42:45 -0400

    trailing whitespace

--- lib/rye/set.rb
+++ lib/rye/set.rb
@@ -1,7 +1,7 @@
 # vim: set sw=2 ts=2 :
 
 module Rye
-  
+
   # = Rye::Set
   #
   #
@@ -9,15 +9,15 @@ module Rye
     attr_reader :name
     attr_reader :boxes
     attr_reader :opts
-    
+
       # Run commands in parallel? A Boolean value. Default: false.
     attr_accessor :parallel
-    
-    
+
+
     # * +name+ The name of the set of machines
-    # * +opts+ a hash of optional arguments 
+    # * +opts+ a hash of optional arguments
     #
-    # The +opts+ hash is used as defaults for all for all Rye::Box objects. 
+    # The +opts+ hash is used as defaults for all for all Rye::Box objects.
     # All args supported by Rye::Box are available here with the addition of:
     #
     # * :parallel => run the commands in parallel? true or false (default).
@@ -25,11 +25,11 @@ module Rye
     def initialize(name='default', opts={})
       @name = name
       @boxes = []
-      
+
       # These opts are use by Rye::Box and also passed to Net::SSH
       @opts = {
         :parallel => false,
-        :user => Rye.sysinfo.user, 
+        :user => Rye.sysinfo.user,
         :safe => true,
         :port => 22,
         :keys => [],
@@ -38,36 +38,36 @@ module Rye
         :debug => nil,
         :error => STDERR,
       }.merge(opts)
-      
+
       @parallel = @opts.delete(:parallel) # Rye::Box doesn't have :parallel
-      
+
       @safe = @opts.delete(:safe)
       @debug = @opts.delete(:debug)
       @error = @opts.delete(:error)
-      
+
       @opts[:keys] = [@opts[:keys]].flatten.compact
-      
+
       add_keys(@opts[:keys])
     end
-    
+
     def opts; @opts; end
     def user; (@opts || {})[:user]; end
     def root?; user.to_s == "root" end
-    
-    # * +boxes+ one or more boxes. Rye::Box objects will be added directly 
-    # to the set. Hostnames will be used to create new instances of Rye::Box 
-    # and those will be added to the list. 
+
+    # * +boxes+ one or more boxes. Rye::Box objects will be added directly
+    # to the set. Hostnames will be used to create new instances of Rye::Box
+    # and those will be added to the list.
     def add_box(*boxes)
-      boxes = boxes.flatten.compact 
+      boxes = boxes.flatten.compact
       @boxes += boxes.collect do |box|
-        box = Rye::Box.new(box, @opts) if box.is_a?(String) 
+        box = Rye::Box.new(box, @opts) if box.is_a?(String)
         box.add_keys(@keys)
         box
       end
       self
     end
     alias :add_boxes :add_box
-    
+
     # Add one or more private keys to each box. Also stores key paths
     # in the set so when new boxes are added they will get the same keys,
     # * +additional_keys+ is a list of file paths to private keys
@@ -83,7 +83,7 @@ module Rye
       self
     end
     alias :add_key :add_keys
-    
+
     def remove_keys(*keys)
       @opts[:keys] ||= []
       @opts[:keys] -= keys.flatten.compact
@@ -93,7 +93,7 @@ module Rye
       self
     end
     alias :remove_key :remove_keys
-    
+
     # Add an environment variable. +n+ and +v+ are the name and value.
     # Returns the instance of Rye::Set
     def setenv(n, v)
@@ -101,21 +101,21 @@ module Rye
       self
     end
     alias :setenvironment_variable :setenv
-    
+
     # See Rye.keys
     def keys
       Rye.keys
     end
-    
+
     def to_s
       "%s:%s" % [self.class.to_s, @name]
     end
-    
+
     def inspect
       a = [self.class.to_s, @name, @parallel, @opts.inspect, @boxes.inspect]
       %q{#<%s:%s parallel=%s opts=%s boxes=%s>} % a
     end
-    
+
     # See Rye::Box.[]
     def [](key=nil)
       run_command(:cd, key)
@@ -126,20 +126,20 @@ module Rye
       run_command(:cd, key)
       self
     end
-    
+
     # Are there any boxes in this set?
     def empty?
       @boxes.nil? || @boxes.empty?
     end
-    
-    # Catches calls to Rye::Box commands. If +meth+ is the name of an 
-    # instance method defined in Rye::Cmd then we call it against all 
+
+    # Catches calls to Rye::Box commands. If +meth+ is the name of an
+    # instance method defined in Rye::Cmd then we call it against all
     # the boxes in +@boxes+. Otherwise this method raises a
     # Rye::CommandNotFound exception. It will also raise a Rye::NoBoxes
-    # exception if this set has no boxes defined. 
+    # exception if this set has no boxes defined.
     #
-    # Returns a Rye::Rap object containing the responses from each Rye::Box. 
-    def method_missing(meth, *args, &block) 
+    # Returns a Rye::Rap object containing the responses from each Rye::Box.
+    def method_missing(meth, *args, &block)
       # Ruby 1.8 populates Module.instance_methods with Strings. 1.9 uses Symbols.
       meth = (Rye.sysinfo.ruby[1] == 8) ? meth.to_s : meth.to_sym
       raise Rye::NoBoxes if @boxes.empty?
@@ -148,39 +148,39 @@ module Rye
       end
       run_command(meth, *args, &block)
     end
-    
+
   private
-    
-    # Determines whether to call the serial or parallel method, then calls it. 
+
+    # Determines whether to call the serial or parallel method, then calls it.
     def run_command(meth, *args, &block)
       runner = @parallel ? :run_command_parallel : :run_command_serial
       self.send(runner, meth, *args, &block)
     end
-    
-    
+
+
     # Run the command on all boxes in parallel
     def run_command_parallel(meth, *args, &block)
       debug "P: #{meth} on #{@boxes.size} boxes (#{@boxes.collect {|b| b.host }.join(', ')})"
       threads = []
-      
+
       raps = Rye::Rap.new(self)
       (@boxes || []).each do |box|
         threads << Thread.new do
           Thread.current[:rap] = box.send(meth, *args, &block) # Store the result in the thread
         end
       end
-      
+
       # Should it bubble up the exception for a single box? socket errors?
-      threads.each do |t| 
+      threads.each do |t|
         Kernel.sleep 0.03     # Give the thread some breathing room
         t.join                # Wait for the thread to finish
         raps << t[:rap]       # Grab the result
       end
-      
+
       raps
     end
-    
-    
+
+
     # Run the command on all boxes in serial
     def run_command_serial(meth, *args, &block)
       debug "S: #{meth} on #{@boxes.size} boxes (#{@boxes.collect {|b| b.host }.join(', ')})"
@@ -190,10 +190,10 @@ module Rye
       end
       raps
     end
-    
+
     def debug(msg); @debug.puts msg if @debug; end
     def error(msg); @error.puts msg if @error; end
-  
+
   end
-  
+
 end
