1f6abca720bed5652e2c2a08561109bd4963d008
Dan Richert <dan.richert@gmail.com>
2012-07-05 15:45:04 -0400

    Pass :safe, :debug, and :error options to boxes

--- try/13_set_tryouts.rb
+++ try/13_set_tryouts.rb
@@ -1,5 +1,16 @@
 require "rye"
 
+@dev_null = File.open("/dev/null", "w")
+
+@opt_test_set = Rye::Set.new(
+  "option passing test",
+  :safe  => false,
+  :debug => @dev_null,
+  :error => @dev_null
+)
+@opt_test_set.add_boxes("11.22.33.44")
+@opt_test_box = @opt_test_set.boxes.first
+
 ## save any raised exceptions
 set = Rye::Set.new("set test", :parallel => true)
 set.add_boxes("localhost", "_")
@@ -11,3 +22,19 @@ set = Rye::Set.new("set test", :parallel => true)
 set.add_boxes("localhost", "_")
 set.hostname.first.first.class
 #=> String
+
+## Pass :safe option to boxes
+#box = @opt_test_set.boxes.first
+@opt_test_box.instance_variable_get(:@rye_safe)
+#=> false
+
+## Pass :debug option to boxes
+@opt_test_box.instance_variable_get(:@rye_debug).path
+#=> "/dev/null"
+
+## Pass :error option to boxes
+@opt_test_box.instance_variable_get(:@rye_error).path
+#=> "/dev/null"
+
+
+@dev_null.close
